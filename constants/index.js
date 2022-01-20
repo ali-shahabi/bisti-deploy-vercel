f034e7fb8aa54d8ee1c2feb1c307ac96aa373b74
Ali Shahabi <shahabiali@outlook.com>
2022-01-20 00:08:13 -0800

    commit

--- constants/index.js
+++
@@ -1,7 +0,0 @@
-export default Object.freeze({
-  BASE_URL: 'https://nosheno.com/',
-  BASE_API_URL: 'https://nosheno.com/api/v0/',
-  BASE_MUSIC_URL: 'https://nosheno.com/music/',
-  BASE_IMAGE_URL: 'https://nosheno.com/image/',
-  REVALIDATE_API_TIME: 10, //milliseconds,
-});
